5b0e54eeeed7d027cc925b138bf98a7ae9036af5
CapedCrusaderD31taf04c3 <shriharikoli25@gmail.com>
2024-03-14 20:56:02 +0530

    copyrights added

--- src/webhooks/custom_webhooks.py
+++ src/webhooks/custom_webhooks.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2024 CapedCrusader
+# All rights reserved.
+#
+# This software is licensed under the terms of the MIT.
+# You should have received a copy of the license along with this
+# program. If not, see LICENSE File.
+# 
+# ======================================
+
 from requests import request
 
 class Webhook:
